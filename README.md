1e19aa29e4de750c5b26349cc8feebad84ed06ae
muhammad-shahin <muhammadshahin002@gmail.com>
2023-09-26 22:43:04 +0600

    readme file edited

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# Donation Campaign Website
 
-Currently, two official plugins are available:
+Welcome to Donation Campaign Website, Donation Campaign is React Base Website where it's user can donate for different categories. Here are some of the top features that make our website stand out:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## 🌟 Donation Categories
+
+- 🍲 **Food Donation**: Feed the hungry and make a difference in someone's life.
+- 🏥 **Healthcare Donation**: Provide medical assistance and improve lives.
+- 👕 **Clothing Donation**: Keep someone warm and stylish with your generous donation.
+- 📚 **Education Donation**: Help build a brighter future through education.
+
+## 🏠 Home Page
+
+- Explore a visually appealing layout with 12 cards, 3 for each category.
+- Click on a card to access specific donation details for that category.
+
+## 💖 Donation Page
+
+- Keep track of your donations on a dedicated page.
+- A friendly modal encourages you to make your first donation if none exists.
+- Click on the modal to return to the Home Page and make your contribution.
+
+## 📊 Statistics Page
+
+- Visualize your impact with a colorful Pie Chart.
+- See the total amount donated against your personal donations in percentage.
+
+## 🌐 Responsive Design
+
+- Access the website seamlessly on various devices and screen sizes.
+- Mobile-friendly design ensures a great experience on smartphones and tablets.
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -8,7 +8,7 @@ const data = [
   { name: "Group A", value: storedDonatedData.length || 0 },
 ];
 
-const COLORS = ["#0088FE", "#00C49F"];
+const COLORS = ["#FF444A", "#00C49F"];
 
 const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = ({
@@ -38,24 +38,42 @@ const renderCustomizedLabel = ({
 
 const App = () => {
   return (
-    <div className="lg:w-fit w-[90%] cursor-pointer container mx-auto">
-      <PieChart width={400} height={400}>
-        <Pie
-          data={data}
-          cx="50%"
-          cy="50%"
-          labelLine={false}
-          label={renderCustomizedLabel}
-          outerRadius={150}
-          fill="#8884d8"
-          dataKey="value"
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-      </PieChart>
-    </div>
+    <section className="lg:w-full w-[90%] cursor-pointer container mx-auto">
+      <h1 className="text-[2rem] text-[#79C281] text-center mt-4">
+        Your Donation Against Total Donation
+      </h1>
+      <div className="w-fit mx-auto">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={150}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+        <div className="flex lg:flex-row flex-col justify-center items-center">
+          <span className="text-[18px] font-medium mr-5 ">
+            Your Donation
+            <button className="w-[100px] h-[12px] bg-[#00C49F] ml-2"></button>
+          </span>
+          <span className="text-[18px] font-medium ">
+            Total Donation
+            <button className="w-[100px] h-[12px] bg-[#FF444A] ml-2"></button>
+          </span>
+        </div>
+      </div>
+    </section>
   );
 };
 
